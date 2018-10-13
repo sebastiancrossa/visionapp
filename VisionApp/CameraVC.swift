a5a16691fd6bd6d23358e466690d0c2045786027
Sebastian C <crossasebastian@gmail.com>
2018-10-13 04:15:35 -0500

    Initial commit

--- VisionApp/CameraVC.swift
+++ VisionApp/CameraVC.swift
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CameraVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
 
 }
 
