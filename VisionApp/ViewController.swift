a9acd3c503086dcf9527d88b5000c3438b432c80
Sebastian C <crossasebastian@gmail.com>
2018-10-13 04:06:26 -0500

    Initial Commit

---
+++ VisionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisionApp
+//
+//  Created by Sebastian Crossa on 10/13/18.
+//  Copyright © 2018 Sebastian Crossa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
